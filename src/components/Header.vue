3691763c1cfa6d9229c2433899046e956c6a71fb
iamaliataie <iamaliataie@gmail.com>
2023-05-04 07:42:41 +0430

    header change, logo change

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
                 <p class="font-bold text-5xl sm:text-6xl md:text-5xl lg:text-7xl text-center md:text-left">Building<br>beautiful web experiences.</p>
                 <div class="flex flex-row items-center justify-center md:justify-start space-x-6">
                     <a href="#about" class="bg-purple-500 px-3 py-2 rounded-md text-slate-50 hover:bg-purple-700 transition duration-200">More about me</a>
-                    <a href="#about" class="px-3 py-2 rounded-md text-purple-500 border-2 border-purple-500 hover:text-slate-50 hover:border-purple-700 hover:bg-purple-700 transition duration-200">Download CV</a>
+                    <!-- <a href="#about" class="px-3 py-2 rounded-md text-purple-500 border-2 border-purple-500 hover:text-slate-50 hover:border-purple-700 hover:bg-purple-700 transition duration-200">Download CV</a> -->
                 </div>
             </div>
             <img src="../assets/header-pic.png" alt="ali" class="w-full sm:w-3/4 md:w-1/2 self-center">
