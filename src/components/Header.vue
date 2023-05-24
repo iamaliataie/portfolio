544ced361d3eb8c0a1c6b325b38f7ace7b59d058
iamaliataie <iamaliataie@gmail.com>
2023-05-24 11:21:35 +0430

    download cv button added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
                 <p class="font-bold text-5xl sm:text-6xl md:text-5xl lg:text-7xl text-center md:text-left"><img src="../assets/favicon.svg" alt="" class="sm:hidden w-10 inline"> Building<br>beautiful web experiences.</p>
                 <div class="flex flex-row items-center justify-center md:justify-start space-x-6">
                     <a href="#about" class="bg-main px-3 py-2 rounded-md text-slate-50 hover:bg-main/90 transition duration-200">More about me</a>
-                    <!-- <a href="#about" class="px-3 py-2 rounded-md text-main hover:text-slate-50 hover:bg-main transition duration-200">Download CV</a> -->
+                    <a href="CV-AliAhmadAtaie.pdf" target="_blank" class="px-3 py-2 rounded-md text-main hover:text-slate-50 hover:bg-main transition duration-200">Download CV</a>
                 </div>
             </div>
             <img src="../assets/header-image.png" alt="ali" class="w-full hidden sm:flex sm:w-3/4 md:w-1/2 lg:w-[500px] self-center">
