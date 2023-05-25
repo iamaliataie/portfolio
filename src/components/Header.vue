ed5069cb7e6b4373c834ca9d5ffdc3a0bdc457d9
iamaliataie <iamaliataie@gmail.com>
2023-05-25 15:58:36 +0430

    about section modified

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
     <section id="home" class="pb-3">
-        <div class="relative section container xl:max-w-6xl min-h-[550px] flex flex-col items-center justify-center gap-10 pt-20 md:pt-0">
+        <div class="relative section container xl:max-w-6xl min-h-screen md:min-h-[550px] flex flex-col items-center justify-center gap-10 pt-20 md:pt-0">
             <div class="flex flex-col-reverse md:flex-row gap-6 items-center justify-between w-full">
                 <div class="text-center md:text-left">
                     <span class="text-xl">Hello, I'm</span>
-                    <h1 class="text-3xl sm:text-5xl md:text-5xl lg:text-7xl font-bold">Ali Ahmad <span class="text-main">Ataie</span></h1>
-                    <h2 class="text-xl sm:text-3xl md:text-3xl lowercase">a web developer</h2>
+                    <h1 class="text-4xl sm:text-5xl lg:text-7xl font-bold">Ali Ahmad <span class="text-main">Ataie</span></h1>
+                    <h2 class="text-2xl sm:text-3xl lowercase">a web developer</h2>
                     <div class="flex flex-row gap-4 items-center justify-center md:justify-start mt-8 md:mt-4">
                         <a href="#contact" class="text-sm px-3 py-2 rounded-md bg-main text-white">Hire me</a>
                         <a href="#portfolio" class="text-sm px-3 py-2 rounded-md bg-main text-white">View my portfolio</a>
@@ -15,7 +15,7 @@
                 <img src="../assets/ataie.jpg" alt="ataie" class="w-full h-full object-cover rounded-md">
               </div>
             </div>
-            <div class="md:absolute bottom-6 left-0 flex flex-col-reverse md:flex-row items-center justify-between container">
+            <div class="md:absolute bottom-6 left-0 flex flex-col space-y-2 md:flex-row items-center justify-between container">
               <div class="flex flex-row items-center gap-4 text-2xl">
                 <a href="https://github.com/iamaliataie" target="_blank"><i class="fab fa-github"></i></a>
                 <a href="https://linkedin.com/in/iamaliataie" target="_blank"><i class="fab fa-linkedin"></i></a>
