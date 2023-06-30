c1799d0bb839cd7af9373948811a7e60aca8db02
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-06-30 18:16:25 +0430

    add custom.js file to home page 3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,7 +28,6 @@ const fetchData = async () => {
 onMounted(() => {
     let navbarScript = document.createElement('script');
     navbarScript.setAttribute('src', '/src/custom.js');
-    navbarScript.setAttribute('type', 'module');
     console.log(navbarScript);
     document.head.appendChild(navbarScript);
     fetchData();
