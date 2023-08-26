95f60bbc526a9859bfc739aa085d96eb62882da6
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-08-26 17:55:46 +0430

    navbar modified

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,14 +4,14 @@
             <div class="flex flex-col-reverse md:flex-row gap-6 items-center justify-between w-full">
                 <div class="text-center md:text-left">
                     <span class="text-xl">Hello, I'm</span>
-                    <h1 class="text-4xl sm:text-5xl lg:text-7xl font-bold">Ali Ahmad <span class="text-main">Ataie</span></h1>
+                    <h1 class="text-4xl sm:text-5xl lg:text-7xl xl:text-8xl font-bold">Ali Ahmad <span class="text-main">Ataie</span></h1>
                     <h2 class="text-2xl sm:text-3xl lowercase">a web developer</h2>
                     <div class="flex flex-row gap-4 items-center justify-center md:justify-start mt-8 md:mt-4">
                         <a href="#contact" class="text-sm px-3 py-2 rounded-md bg-main text-white">Hire me</a>
                         <a href="#portfolio" class="text-sm px-3 py-2 rounded-md bg-main text-white">View my portfolio</a>
                     </div>
               </div>
-              <div class="hidden md:flex w-full h-60 sm:w-96 md:h-full md:w-2/6 lg:w-2/6  p-2 md:p-0">
+              <div class="hidden md:flex w-full h-60 sm:w-96 md:h-full md:w-2/6 lg:w-2/6 xl:w-5/12  p-2 md:p-0">
                 <img src="../assets/ataie.jpg" alt="ataie" class="w-full h-full object-cover rounded-md">
               </div>
             </div>
