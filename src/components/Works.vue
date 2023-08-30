0a40c0e8d36d63ed1425895ef73ec066e52988bd
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-08-30 10:04:00 +0430

    Update Works.vue

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -10,8 +10,8 @@ const props = defineProps(['loading', 'works'])
     <section id="portfolio" class="py-2" >
         <div class="section container border-b-2 dark:border-gray-900 xl:max-w-7xl w-11/12 px-0 py-20 space-y-8">
             <header class="flex flex-col text-center">
-                <h6 class="uppercase text-main font-bold">award winning projects</h6>
-                <h1 class="text-[30px] md:text-[40px] lg:text-[50px] font-bold">Latest Projects</h1>
+                <h1 class="uppercase text-main font-bold">award winning projects</h6>
+                <h2 class="text-[30px] md:text-[40px] lg:text-[50px] font-bold">Latest Projects</h1>
             </header>
             <div
                 v-if="loading"
