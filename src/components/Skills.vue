f55f6a1a89745e2845cb8d4c0a5f77a276b1e057
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-08-30 11:00:53 +0430

    Update Skills.vue

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -7,35 +7,35 @@
             </header>
             <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 sm:gap-8">
                 <div v-motion-slide-visible-once-bottom class="flex flex-row items-center justify-center gap-2 sm:gap-4 shadow-md bg-slate-50 shadow-slate-300 dark:shadow-black dark:bg-slate-900 py-8 rounded-lg">
-                    <img src="../assets/skills/python.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 sm:h-fit dark:filter-none">
+                    <img src="../assets/skills/python.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 h-fit dark:filter-none">
                     <span class="text-lg sm:text-2xl font-bold">Python</span>
                 </div>
                 <div v-motion-slide-visible-once-bottom class="flex flex-row items-center justify-center gap-2 sm:gap-4 shadow-md bg-slate-50 shadow-slate-300 dark:shadow-black dark:bg-slate-900 py-8 rounded-lg">
-                    <img src="../assets/skills/django.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 sm:h-10 dark:filter-none">
+                    <img src="../assets/skills/django.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 h-fit dark:filter-none">
                     <span class="text-lg sm:text-2xl font-bold">Django</span>
                 </div>
                 <div v-motion-slide-visible-once-bottom class="flex flex-row items-center justify-center gap-2 sm:gap-4 shadow-md bg-slate-50 shadow-slate-300 dark:shadow-black dark:bg-slate-900 py-8 rounded-lg">
-                    <img src="../assets/skills/js.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 sm:h-10 dark:filter-none">
+                    <img src="../assets/skills/js.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 h-fit dark:filter-none">
                     <span class="text-lg sm:text-2xl font-bold">JavaScript</span>
                 </div>
                 <div v-motion-slide-visible-once-bottom class="flex flex-row items-center justify-center gap-2 sm:gap-4 shadow-md bg-slate-50 shadow-slate-300 dark:shadow-black dark:bg-slate-900 py-8 rounded-lg">
-                    <img src="../assets/skills/ts.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 sm:h-10 dark:filter-none">
+                    <img src="../assets/skills/ts.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 h-fit dark:filter-none">
                     <span class="text-lg sm:text-2xl font-bold">TypeScript</span>
                 </div>       
                 <div v-motion-slide-visible-once-bottom class="flex flex-row items-center justify-center gap-2 sm:gap-4 shadow-md bg-slate-50 shadow-slate-300 dark:shadow-black dark:bg-slate-900 py-8 rounded-lg">
-                    <img src="../assets/skills/vue.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 sm:h-10 dark:filter-none">
+                    <img src="../assets/skills/vue.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 h-fit dark:filter-none">
                     <span class="text-lg sm:text-2xl font-bold">VueJS</span>
                 </div>
                 <div v-motion-slide-visible-once-bottom class="flex flex-row items-center justify-center gap-2 sm:gap-4 shadow-md bg-slate-50 shadow-slate-300 dark:shadow-black dark:bg-slate-900 py-8 rounded-lg">
-                    <img src="../assets/skills/tailwind.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 sm:h-10 dark:filter-none">
+                    <img src="../assets/skills/tailwind.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 h-fit dark:filter-none">
                     <span class="text-lg sm:text-2xl font-bold">TailwindCSS</span>
                 </div>
                 <div v-motion-slide-visible-once-bottom class="flex flex-row items-center justify-center gap-2 sm:gap-4 shadow-md bg-slate-50 shadow-slate-300 dark:shadow-black dark:bg-slate-900 py-8 rounded-lg">
-                    <img src="../assets/skills/supabase.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 sm:h-10 dark:filter-none">
+                    <img src="../assets/skills/supabase.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 h-fit dark:filter-none">
                     <span class="text-lg sm:text-2xl font-bold">Supabase</span>
                 </div>
                 <div v-motion-slide-visible-once-bottom class="flex flex-row items-center justify-center gap-2 sm:gap-4 shadow-md bg-slate-50 shadow-slate-300 dark:shadow-black dark:bg-slate-900 py-8 rounded-lg">
-                    <img src="../assets/skills/bootstrap.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 sm:h-10 dark:filter-none">
+                    <img src="../assets/skills/bootstrap.png" alt="Python" class="filter invert w-6 h-6 sm:w-10 h-fit dark:filter-none">
                     <span class="text-lg sm:text-2xl font-bold">Bootstrap</span>
                 </div>
             </div>
