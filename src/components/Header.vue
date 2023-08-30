2279077aa24af45e8e8e4a7b67b7a04588314704
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-08-30 09:26:55 +0430

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,8 +7,12 @@
                     <h1 class="text-4xl sm:text-5xl lg:text-7xl xl:text-[88px] font-bold">Ali Ahmad <span class="text-main">Ataie</span></h1>
                     <h2 class="text-2xl sm:text-3xl xl:text-5xl lowercase">a web developer</h2>
                     <div class="flex flex-row gap-4 items-center justify-center md:justify-start mt-8 md:mt-4">
-                        <a href="#contact" class="text-sm xl:text-lg px-3 py-2 rounded-md bg-main text-white">Hire me</a>
-                        <a href="#portfolio" class="text-sm xl:text-lg px-3 py-2 rounded-md bg-main text-white">View my portfolio</a>
+                        <a 
+                            aria-label="contact"
+                            href="#contact" class="text-sm xl:text-lg px-3 py-2 rounded-md bg-main text-white">Hire me</a>
+                        <a 
+                            aria-label="portfolio"
+                            href="#portfolio" class="text-sm xl:text-lg px-3 py-2 rounded-md bg-main text-white">View my portfolio</a>
                     </div>
               </div>
               <div class="hidden md:flex w-full h-60 sm:w-96 md:h-full md:w-2/6 lg:w-2/6 xl:w-5/12  p-2 md:p-0">
@@ -17,11 +21,15 @@
             </div>
             <div class="md:absolute bottom-6 left-0 flex flex-col space-y-2 md:flex-row items-center justify-between container">
               <div class="flex flex-row items-center gap-4 text-2xl xl:text-3xl">
-                <a href="https://github.com/iamaliataie" target="_blank"><i class="fab fa-github"></i></a>
-                <a href="https://linkedin.com/in/iamaliataie" target="_blank"><i class="fab fa-linkedin"></i></a>
-                <a href="https://instagram.com/ataie.dev" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a aria-label="link to github"
+                    href="https://github.com/iamaliataie" target="_blank"><i class="fab fa-github"></i></a>
+                <a aria-label="link to linkedin"
+                    href="https://linkedin.com/in/iamaliataie" target="_blank"><i class="fab fa-linkedin"></i></a>
+                <a aria-label="link to instagram"
+                    href="https://instagram.com/ataie.dev" target="_blank"><i class="fab fa-instagram"></i></a>
               </div>
-              <a href="mailto:hello@ataie.dev" class="text-lg xl:text-2xl">hello@ataie.dev</a>
+              <a aria-label="email address"
+                  href="mailto:hello@ataie.dev" class="text-lg xl:text-2xl">hello@ataie.dev</a>
             </div>
         </div>
     </section>
