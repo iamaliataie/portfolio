f4982df5a2d718b692f21f9fa33c4c001ea13438
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-09-02 17:20:43 +0430

    update vit config for pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,48 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        cleanupOutdatedCaches: true,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
+      },
+      manifest: {
+        name: 'Ali Ahmad Ataie',
+        short_name: 'porftolio',
+        description: 'brief description about ali ahmad ataie',
+        theme_color: '#0369C7',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'image/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'image/icons/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png'
+          },
+          {
+            src: 'image/icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          {
+            src: 'image/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+        ]
+      }
+    })
+  ],
 })
