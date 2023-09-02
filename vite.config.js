04f83aba45698e00aae52707507c20783e33b57d
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-09-02 16:49:35 +0430

    update theme color

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [vue()],
 })
