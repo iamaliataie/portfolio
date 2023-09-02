3d3f248e3f58dc5e1260318858474d576ae8dc22
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-09-02 17:31:52 +0430

    update vit config for pwa

--- vite.config.js
+++ vite.config.js
@@ -22,22 +22,22 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'image/icons/icon-192x192.png',
+            src: 'logo.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'image/icons/icon-256x256.png',
+            src: 'logo.png',
             sizes: '256x256',
             type: 'image/png'
           },
           {
-            src: 'image/icons/icon-384x384.png',
+            src: 'logo.png',
             sizes: '384x384',
             type: 'image/png'
           },
           {
-            src: 'image/icons/icon-512x512.png',
+            src: 'logo.png',
             sizes: '512x512',
             type: 'image/png'
           },
