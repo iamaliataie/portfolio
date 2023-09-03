1d220a013989d34cd5a1106a722ec1300d46ce77
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-09-03 16:05:33 +0430

    pwa added

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Ataie Portfolio",
+        short_name: "Portfolio",
+        icons: [
+          {
+            src: "/512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
+          }
+        ]
+      },
+    })
   ],
 })
