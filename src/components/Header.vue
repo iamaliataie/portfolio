1758db40ea060121454adcff5e9158fd5f02e483
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-09-03 07:14:39 +0430

    update header section

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,19 +2,19 @@
     <section id="home" class="min-h-screen md:h-auto pt-[80px]">
         <div class="section container h-full py-10 md:py-24">
             <div
-                class="grid grid-cols-1 md:grid-cols-2 md:rounded-3xl h-full xl:min-h-[600px] overflow-hidden space-y-10 md:shadow-xl md:space-y-0 md:bg-white dark:bg-slate-900 z-50">
+                class="grid grid-cols-1 md:grid-cols-2 md:rounded-3xl h-full xl:min-h-[600px] overflow-hidden space-y-10 md:shadow-xl md:space-y-0 md:bg-white md:dark:bg-slate-900 z-50">
                 <div
                     class="flex flex-col justify-center items-center space-y-12 px-6 sm:px-20 sm:py-10 md:px-8 lg:px-14 lx:px-20">
                     <div class="text-center md:text-start w-full">
                         <p
-                            class="text-lg sm:text-3xl md:text-xl lg:text-2xl font-bold mb-2 text-slate-800 dark:text-slate-300">
+                            class="text-lg sm:text-3xl md:text-xl lg:text-2xl font-bold mb-2 text-slate-800 dark:text-slate-100">
                             Welcome, I am</p>
                         <h1
-                            class="text-4xl sm:text-6xl md:text-4xl lg:text-5xl font-bold xl:text-6xl text-slate-900 dark:text-slate-200">
+                            class="text-4xl sm:text-6xl md:text-4xl lg:text-5xl font-bold xl:text-6xl text-slate-900 dark:text-slate-50">
                             Ali Ahmad Ataie</h1>
                         <h2 class="text-2xl sm:text-4xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold text-sky-700">Full
                             Stack Developer</h2>
-                        <p class="text-justify sm:text-lg md:text-sm lg:text-lg mt-4 text-slate-800 dark:text-slate-500">
+                        <p class="text-justify sm:text-lg md:text-sm lg:text-lg mt-4 text-slate-800 dark:text-slate-300">
                             I am a web Developer with a vast array of knowledge in many different frontend and backend
                             languages, responsive frameworks, databases, and best code practices.
                         </p>
