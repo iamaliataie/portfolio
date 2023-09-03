e5dd767649b8c7113c891b0af786868ecae010f8
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-09-03 21:34:00 +0430

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
+        cleanupOutdatedCaches: true,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,pdf}']
       },
       manifest: {
