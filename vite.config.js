386b8f52297f7f1d4fdac782e852f720869c54cc
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-09-05 08:19:54 +0430

    commented pwa

--- vite.config.js
+++ vite.config.js
@@ -7,62 +7,62 @@ export default defineConfig({
   base: '/',
   plugins: [
     vue(),
-    VitePWA({
-      registerType: 'prompt',
-      workbox: {
-        cleanupOutdatedCaches: true,
-        skipWaiting: true,
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,pdf}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fasxyiajvwstushdwzxo\.supabase\.co\/.*/i,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'supabase-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
-              },
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          },
-        ]
-      },
-      manifest: {
-        name: "Ali Ahmad Ataie",
-        short_name: "Ali's Portfolio",
-        icons: [
-          {
-            src: "/icons/192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icons/256.png",
-            sizes: "256x256",
-            type: "image/png",
-          },
-          {
-            src: "/icons/384.png",
-            sizes: "384x384",
-            type: "image/png",
-          },
-          {
-            src: "/icons/512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any"
-          },
-          {
-            src: "/icons/maskable_icon.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable"
-          },
-        ]
-      },
-    })
+    // VitePWA({
+    //   registerType: 'prompt',
+    //   workbox: {
+    //     cleanupOutdatedCaches: true,
+    //     skipWaiting: true,
+    //     globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,pdf}'],
+    //     runtimeCaching: [
+    //       {
+    //         urlPattern: /^https:\/\/fasxyiajvwstushdwzxo\.supabase\.co\/.*/i,
+    //         handler: 'NetworkFirst',
+    //         options: {
+    //           cacheName: 'supabase-cache',
+    //           expiration: {
+    //             maxEntries: 10,
+    //             maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+    //           },
+    //           cacheableResponse: {
+    //             statuses: [0, 200]
+    //           }
+    //         }
+    //       },
+    //     ]
+    //   },
+    //   manifest: {
+    //     name: "Ali Ahmad Ataie",
+    //     short_name: "Ali's Portfolio",
+    //     icons: [
+    //       {
+    //         src: "/icons/192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/icons/256.png",
+    //         sizes: "256x256",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/icons/384.png",
+    //         sizes: "384x384",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/icons/512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //         purpose: "any"
+    //       },
+    //       {
+    //         src: "/icons/maskable_icon.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //         purpose: "any maskable"
+    //       },
+    //     ]
+    //   },
+    // })
   ],
 })
