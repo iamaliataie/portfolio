a9652909d49d9ffcdbe9a00db9515d35f96b801a
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-09-05 08:37:11 +0430

    added theme toggler

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,7 +15,8 @@
                         <h2 class="text-2xl sm:text-4xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold text-sky-700">Full
                             Stack Developer</h2>
                         <p class="text-justify sm:text-lg md:text-sm lg:text-lg mt-4 text-slate-800 dark:text-slate-300">
-                            As a full-stack developer specializing in Python, Django, JavaScript, and Vue.js, my portfolio showcases my skills and expertise in creating robust and dynamic web applications. Explore my projects and see how I can bring your ideas to life.
+                            I am a fullstack Developer with a vast array of knowledge in many different frontend and backend
+                            languages, responsive frameworks, databases, and best code practices.
                         </p>
                     </div>
                     <div class="text-center md:text-start w-full flex items-center space-x-4 justify-center md:justify-start">
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,37 +14,50 @@ const openMenu = ref(false)
             <div class="flex flex-row space-x-2 items-center justify-start">
                 <img src="../assets/logo.svg" class="h-full animate-spin-slow" alt="">
             </div>
-            <button @click="openMenu = !openMenu" class="md:hidden text-2xl">
-                <i class="fa fa-bars"></i>
-            </button>
-            <ul class="absolute top-[80px] left-0 right-0 h-screen space-y-10 text-xl p-10 transition-transform duration-300 ease-in-out md:text-lg md:space-y-0 md:p-0 md:text-md md:static md:h-auto bg-slate-50 dark:bg-slate-950 flex-col flex md:flex-row md:translate-x-0 items-center md:space-x-10 uppercase font-semibold"
-                :class="openMenu ? 'translate-x-0' : 'translate-x-full'">
-                <li class="w-full">
-                    <a @click="openMenu = false" href="#home" class="">Home</a>
-                </li>
-                <li class="w-full">
-                    <a @click="openMenu = false" href="#portfolio" class="">Portfolio</a>
-                </li>
-                <li class="w-full">
-                    <a @click="openMenu = false" href="#skills" class="">Skills</a>
-                </li>
-                <li class="w-full">
-                    <a @click="openMenu = false" href="#contact" class="bg-sky-600 px-3 py-2 text-white rounded-md">Contact</a>
-                </li>
-                <li class="md:hidden">
-                    <div class="flex flex-col space-y-4">
-                        <div class="flex items-center space-x-6 justify-center">
-                            <a href="https://github.com/iamaliataie" target="_blank"><i class="fa-brands fa-github text-sky-600 text-4xl"></i></a>
-                            <a href="https://linkedin.com/in/iamaliataie" target="_blank"><i class="fa-brands fa-linkedin text-sky-600 text-4xl"></i></a>
-                            <a href="https://instagram.com/iamaliataie" target="_blank"><i class="fa-brands fa-instagram text-sky-600 text-4xl"></i></a>
-                        </div>
-                        <div class="text-center">
-                            <span class="text-slate-400 text-sm">&copy; 2023. All Rights Reserved</span>
-                        </div>
-                    </div>
-                </li>
-            </ul>
 
+            <div class="flex items-center items-center space-x-6">
+                <ul class="absolute top-[80px] left-0 right-0 h-screen space-y-10 text-xl p-10 transition-transform duration-300 ease-in-out md:text-lg md:space-y-0 md:p-0 md:text-md md:static md:h-auto bg-slate-50 dark:bg-slate-950 flex-col flex md:flex-row md:translate-x-0 items-center md:space-x-10 font-semibold"
+                :class="openMenu ? 'translate-x-0' : 'translate-x-full'">
+                    <li class="w-full">
+                        <a @click="openMenu = false" href="#home" class="">Home</a>
+                    </li>
+                    <li class="w-full">
+                        <a @click="openMenu = false" href="#portfolio" class="">Portfolio</a>
+                    </li>
+                    <li class="w-full">
+                        <a @click="openMenu = false" href="#skills" class="">Skills</a>
+                    </li>
+                    <li class="w-full">
+                        <a @click="openMenu = false" href="#contact" class="">Contact</a>
+                    </li>
+                    <li class="md:hidden">
+                        <div class="flex flex-col space-y-4">
+                            <div class="flex items-center space-x-6 justify-center">
+                                <a href="https://github.com/iamaliataie" target="_blank"><i class="fa-brands fa-github text-sky-600 text-4xl"></i></a>
+                                <a href="https://linkedin.com/in/iamaliataie" target="_blank"><i class="fa-brands fa-linkedin text-sky-600 text-4xl"></i></a>
+                                <a href="https://instagram.com/iamaliataie" target="_blank"><i class="fa-brands fa-instagram text-sky-600 text-4xl"></i></a>
+                            </div>
+                            <div class="text-center">
+                                <span class="text-slate-400 text-sm">&copy; 2023. All Rights Reserved</span>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+
+                <div>
+                    <button @click="toggleTheme()" v-if="isDark" class="text-2xl md:text-lg">
+                        <i class="fa fa-moon "></i>
+                    </button>
+                    <button @click="toggleTheme()" v-else class="text-2xl md:text-lg">
+                        <i class="fa fa-sun "></i>
+                    </button>
+                </div>
+
+                <button @click="openMenu = !openMenu" class="md:hidden text-2xl">
+                    <i class="fa fa-bars"></i>
+                </button>
+
+            </div>
         </div>
     </header>
 </template>
