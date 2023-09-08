5e5f78a3cc8f9e848becb520ce24494792be1bb3
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-09-08 17:33:20 +0430

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@ const year = new Date().getFullYear();
     <div class="bg-black py-8">
         <div class="container flex flex-col md:flex-row items-center justify-between gap-8">
             <div class="flex flex-col md:flex-row space-x-2 items-center space-y-4 md:space-y-0">
-                <img src="../assets/logo.svg" class="w-16 md:w-auto" alt="">
+                <img src="../assets/logo.svg" class="w-16 h-16 md:w-auto md:h-auto" alt="">
                 <span class=" text-3xl md:text-3xl text-slate-100 font-semibold">Ali Ahmad Ataie</span>
             </div>
             <div class="flex flex-col space-y-4">
