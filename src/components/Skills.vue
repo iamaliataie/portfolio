fca094896f1c0c830719a3caefc67dda51c0c7c7
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-09-08 17:37:09 +0430

    Update Skills width height.vue

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -11,7 +11,7 @@
                 <div
                     class="grid grid-cols-1 sm:grid-cols-4 bg-white dark:bg-slate-900 rounded-lg sm:rounded-2xl overflow-hidden shadow-xl dark:border dark:border-slate-700">
                     <div class="col-span-1 p-4 sm:p-2">
-                        <img src="../assets/sk/python.png" alt="">
+                        <img src="../assets/sk/python.png" alt="" class="w-fit h-fit" >
                     </div>
                     <div
                         class="col-span-3 bg-sky-800 py-2 sm:p-0 sm:rounded-xl flex justify-center items-center w-full h-full">
