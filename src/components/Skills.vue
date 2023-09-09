c567a89a754fdb43679b40107b0b928289ce054f
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-09-09 17:21:58 +0430

    Update Skills.vue

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -31,7 +31,7 @@
                 <div
                     class="grid grid-cols-1 sm:grid-cols-4 bg-white dark:bg-slate-900 rounded-lg sm:rounded-2xl overflow-hidden shadow-xl dark:border dark:border-slate-700">
                     <div class="col-span-1 p-4 sm:p-2">
-                        <img src="../assets/sk/js.png" alt="" class="w-fit h-fit" >
+                        <img src="../assets/sk/js.png" alt="" class="w-fit h-fit">
                     </div>
                     <div
                         class="col-span-3 bg-yellow-300 py-2 sm:p-0 sm:rounded-xl flex justify-center items-center w-full h-full">
