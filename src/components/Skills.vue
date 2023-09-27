9a1f08acfa1dba2af0539bba1912423f9deb8f8a
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-09-27 18:34:09 +0430

    Update Skills.vue

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -81,7 +81,7 @@
                 <div
                     class="grid grid-cols-1 sm:grid-cols-4 bg-white dark:bg-slate-900 rounded-lg sm:rounded-2xl overflow-hidden shadow-xl dark:border dark:border-slate-700">
                     <div class="col-span-1 p-4 sm:p-2">
-                        <img src="../assets/sk/postgres.png" alt="" class="w-fit h-full">
+                        <img src="../assets/sk/postgres.png" alt="postgresql" class="w-fit h-full">
                     </div>
                     <div
                         class="col-span-3 bg-cyan-700 py-2 sm:p-0  sm:rounded-xl flex justify-center items-center w-full h-full">
