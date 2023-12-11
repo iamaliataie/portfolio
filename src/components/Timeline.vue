30735ffea74598accef116f28b13c95e4fb28671
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-12-11 20:49:20 +0430

    Update timeline component

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,3 +1,11 @@
+<script setup>
+import {reactive} from 'vue'
+import TimelineCard from './TimelineCard.vue'
+
+const resume = reactive()
+
+</script>
+
 <template>
     <section id="portfolio" class="py-2 bg-slate-100 dark:bg-slate-950">
         <div class="section container py-32">
@@ -9,27 +17,12 @@
             </div>
             <div class="flex flex-col space-y-10">
                 <div class="grid grid-cols-1 md:grid-cols-2 items-center gap-10">
-                    <div class="p-8 bg-slate-300 dark:bg-slate-800 rounded-xl">
-                        <div class="flex flex-col space-y-2">
-                            <p class="text-xl font-extrabold text-sky-700">Dec 2023 - Present</p>
-                            <p class="font-semibold text-3xl">Odoo Developer</p>
-                            <a class="underline" href="https://netlinks.net" target="_blank">Netlinks Ltd</a>
-                        </div>
-                    </div>
-                    <div class="p-8 bg-slate-300 dark:bg-slate-800 rounded-xl">
-                        <div class="flex flex-col space-y-2">
-                            <p class="text-xl font-extrabold text-sky-700">July 2023 - Nov 2023</p>
-                            <p class="font-semibold text-3xl">Software Developer</p>
-                            <a class="underline" href="https://netlinks.net" target="_blank">Netlinks Ltd</a>
-                        </div>
-                    </div>
-                    <div class="p-8 bg-slate-300 dark:bg-slate-800 rounded-xl">
-                        <div class="flex flex-col space-y-2">
-                            <p class="text-xl font-extrabold text-sky-700">2019 - 2022</p>
-                            <p class="font-semibold text-3xl">Computer Science</p>
-                            <a class="underline" href="https://kateb.edu.af" target="_blank">Kateb University</a>
-                        </div>
-                    </div>
+                    <TimelineCard
+                    v-for="item in resume"
+                    :date=item.date
+                    :title=item.title
+                    :organization=item.organization
+                    />
                 </div>
             </div>
         </div>
