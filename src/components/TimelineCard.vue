3016a6542f0ba78b29f726cfabe3415ab81eb93b
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-12-11 20:27:45 +0430

    Update timeline card component

--- src/components/TimelineCard.vue
+++ src/components/TimelineCard.vue
@@ -1,12 +1,12 @@
 <script setup>
-
+const props = defineProps(['date', 'title', 'organization'])
 </script>
 <template>
     <div class="p-8 bg-slate-300 dark:bg-slate-800 rounded-xl">
         <div class="flex flex-col space-y-2">
-            <p class="text-xl font-extrabold text-sky-700">July 2023 - Nov 2023</p>
-            <p class="font-semibold text-3xl">Software Developer</p>
-            <a class="underline" href="https://netlinks.net" target="_blank">Netlinks Ltd</a>
+            <p class="text-xl font-extrabold text-sky-700">{{ date }}</p>
+            <p class="font-semibold text-3xl">{{ title }}</p>
+            <a class="underline" :href=organization.link target="_blank">{{ organization.title }}</a>
         </div>
     </div>
 </template>
