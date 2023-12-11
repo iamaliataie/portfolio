21c9df87e8dfcea39e465f8d30c96a868e197974
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-12-11 20:14:38 +0430

    Create timeline card component

---
+++ src/components/TimelineCard.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="p-8 bg-slate-300 dark:bg-slate-800 rounded-xl">
+        <div class="flex flex-col space-y-2">
+            <p class="text-xl font-extrabold text-sky-700">July 2023 - Nov 2023</p>
+            <p class="font-semibold text-3xl">Software Developer</p>
+            <a class="underline" href="https://netlinks.net" target="_blank">Netlinks Ltd</a>
+        </div>
+    </div>
+</template>
