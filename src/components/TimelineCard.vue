6c3d93404b13d6f27484d47757aeb7c94cb83da6
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-12-12 15:40:56 +0430

    Update TimelineCard.vue

--- src/components/TimelineCard.vue
+++ src/components/TimelineCard.vue
@@ -1,5 +1,5 @@
 <script setup>
-const props = defineProps(['date', 'title', 'organization_title', 'organization_link'])
+const props = defineProps(['date', 'title', 'organization_title', 'organization_link', 'description'])
 </script>
 <template>
     <div class="p-8 bg-slate-300 dark:bg-slate-800 rounded-xl">
@@ -7,6 +7,7 @@ const props = defineProps(['date', 'title', 'organization_title', 'organization_
             <p class="text-lg sm:text-xl font-extrabold text-sky-700">{{ date }}</p>
             <p class="font-semibold text-[24px] sm:text-3xl ">{{ title }}</p>
             <a class="underline font-semibold" :href=organization_link target="_blank">{{ organization_title }}</a>
+            <p class="text-justify">{{ description }}</p>
         </div>
     </div>
 </template>
