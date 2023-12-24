fd0893e8fec057382647d802a8aa970ec95e97e6
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-12-24 19:51:43 +0430

    Update skill dark background IX

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -89,7 +89,7 @@
                     </div>
                 </div>
                 <div
-                    class="grid grid-cols-1 sm:grid-cols-4 bg-white dark:bg-slate-900 rounded-lg sm:rounded-2xl overflow-hidden shadow-xl dark:border dark:border-slate-700">
+                    class="grid grid-cols-1 sm:grid-cols-4 bg-white dark:bg-slate-950 rounded-lg sm:rounded-2xl overflow-hidden shadow-xl dark:border dark:border-slate-700">
                     <div class="col-span-1 p-4 sm:p-2">
                         <img src="../assets/sk/xd.png" alt="xd" class="w-fit h-full">
                     </div>
